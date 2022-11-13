7d6280a044c8f3626391439706aec6bb9a43c061
Will-Thomas1 <william.thomas31@btopenworld.com>
2022-11-13 14:53:36 +0000

    Now automatically chooses all columns with 'abundance' in them if comparison_cols param is left empty

--- R/normalise_to.R
+++ R/normalise_to.R
@@ -1,5 +1,8 @@
 #' Creates new columns that are normalised to a specified column with abundance values.
 #'
+#'If comparison_cols parameter is left blank all columns that contain 'abundance' in the title will be normalised.
+#'New column names will be the original column name + '/' + reference column name.
+#'
 #' @param df A dataframe with different sample abundance values.
 #' @param reference_col The column name of the abundance values that will become the 'reference values', given as a string.
 #' @param comparison_cols A vector of column names to normalise.
@@ -9,6 +12,14 @@
 #' @export
 
 normalise_to <- function(df, reference_col, comparison_cols){
+
+  if(missing(comparison_cols)) {
+
+    comparison_cols <- df %>%
+      dplyr::select(dplyr::contains("abundance")) %>%
+      colnames(.)
+
+  }
 
   new_col_names <- comparison_cols %>% #Make new col names
     paste(., "/", reference_col, sep = "")
